cb8d9e47821155c087fcfdaea416e97ad0fbf244
Tim0310ne <144581314+Tim0310ne@users.noreply.github.com>
2026-04-28 08:49:33 +0800

    arms and legs

--- project-b-prototype/sketch.js
+++ project-b-prototype/sketch.js
@@ -14,8 +14,11 @@ function draw() {
   robot.update();
   robot.display();
 
-  fill(70);
+  fill(255);
   noStroke();
+  textSize(14);
+  textAlign(CENTER);
+  text("Press A for arms, press L for legs", width / 2, 30);
 }
 
 function mousePressed() {
@@ -35,34 +38,168 @@ class Robot {
     this.x = x;
     this.y = y;
     this.head = new Head(this.x, this.y - 95, 90);
+    this.leftArm = new Limb(this.x - 60, this.y - 45, 20, 70);
+    this.rightArm = new Limb(this.x + 60, this.y - 45, 20, 70);
+    this.leftLeg = new Limb(this.x - 25, this.y + 60, 22, 70);
+    this.rightLeg = new Limb(this.x + 25, this.y + 60, 22, 70);
   }
 
   update() {
     this.head.update();
+    this.leftArm.update();
+    this.rightArm.update();
+    this.leftLeg.update();
+    this.rightLeg.update();
   }
 
   display() {
-    this.displayBody();
+    this.leftArm.display();
+    this.rightArm.display();
+
+    noStroke();
+    fill(132, 156, 194);
+    rect(this.x, this.y, 120, 120, 18);
+
+    this.leftLeg.display();
+    this.rightLeg.display();
     this.head.display();
   }
 
-  displayBody() {
-    noStroke();
-
-    fill(132, 156, 194);
-    rect(this.x, this.y, 120, 120, 18);
-  }
-
   mousePressed(mx, my) {
-    this.head.mousePressed(mx, my);
+    if (this.head.mousePressed(mx, my)) return;
+    if (this.leftArm.mousePressed(mx, my)) return;
+    if (this.rightArm.mousePressed(mx, my)) return;
+    if (this.leftLeg.mousePressed(mx, my)) return;
+    if (this.rightLeg.mousePressed(mx, my)) return;
   }
 
   mouseDragged(mx, my) {
     this.head.mouseDragged(mx, my);
+    this.leftArm.mouseDragged(mx, my);
+    this.rightArm.mouseDragged(mx, my);
+    this.leftLeg.mouseDragged(mx, my);
+    this.rightLeg.mouseDragged(mx, my);
   }
 
   mouseReleased() {
     this.head.mouseReleased();
+    this.leftArm.mouseReleased();
+    this.rightArm.mouseReleased();
+    this.leftLeg.mouseReleased();
+    this.rightLeg.mouseReleased();
+  }
+}
+
+class Limb {
+  constructor(pivotX, pivotY, w, h) {
+    this.pivotX = pivotX;
+    this.pivotY = pivotY;
+    this.w = w;
+    this.h = h;
+    this.baseX = pivotX;
+    this.baseY = pivotY + 35;
+    this.x = this.baseX;
+    this.y = this.baseY;
+
+    this.angle = 0;
+    this.angularVelocity = 0;
+    this.velocityX = 0;
+    this.velocityY = 0;
+    this.dragging = false;
+    this.dragOffsetX = 0;
+    this.dragOffsetY = 0;
+    this.lastDragX = this.baseX;
+    this.lastDragY = this.baseY;
+  }
+
+  update() {
+    if (this.dragging) {
+      this.angle = constrain(this.angle, -0.9, 0.9);
+      return;
+    }
+
+    let springX = (this.baseX - this.x) * 0.12;
+    let springY = (this.baseY - this.y) * 0.12;
+    let damping = 0.84;
+
+    this.velocityX += springX;
+    this.velocityY += springY;
+    this.velocityX *= damping;
+    this.velocityY *= damping;
+
+    this.x += this.velocityX;
+    this.y += this.velocityY;
+
+    this.angularVelocity += this.velocityX * 0.03 - this.angle * 0.16;
+    this.angularVelocity *= 0.78;
+    this.angle += this.angularVelocity;
+
+    if (
+      abs(this.baseX - this.x) < 0.01 &&
+      abs(this.baseY - this.y) < 0.01 &&
+      abs(this.velocityX) < 0.01 &&
+      abs(this.velocityY) < 0.01
+    ) {
+      this.x = this.baseX;
+      this.y = this.baseY;
+      this.velocityX = 0;
+      this.velocityY = 0;
+      this.angularVelocity = 0;
+    }
+
+    this.angle = constrain(this.angle, -0.9, 0.9);
+  }
+
+  display() {
+    push();
+    translate(this.pivotX, this.pivotY);
+    rotate(this.angle);
+    translate(this.x - this.pivotX, this.y - this.pivotY);
+    noStroke();
+    fill(132, 156, 194);
+    rect(0, 0, this.w, this.h, 10);
+    pop();
+  }
+
+  mousePressed(mx, my) {
+    if (dist(mx, my, this.x, this.y) < max(this.w, this.h) * 0.55) {
+      this.dragging = true;
+      this.dragOffsetX = mx - this.x;
+      this.dragOffsetY = my - this.y;
+      this.lastDragX = this.x;
+      this.lastDragY = this.y;
+      this.velocityX = 0;
+      this.velocityY = 0;
+      this.angularVelocity = 0;
+      return true;
+    }
+    return false;
+  }
+
+  mouseDragged(mx, my) {
+    if (!this.dragging) {
+      return;
+    }
+
+    this.lastDragX = this.x;
+    this.lastDragY = this.y;
+
+    this.x = constrain(mx - this.dragOffsetX, this.baseX - 42, this.baseX + 42);
+    this.y = constrain(my - this.dragOffsetY, this.baseY - 42, this.baseY + 42);
+
+    this.velocityX = this.x - this.lastDragX;
+    this.velocityY = this.y - this.lastDragY;
+  }
+
+  mouseReleased() {
+    if (!this.dragging) {
+      return;
+    }
+
+    this.dragging = false;
+    this.velocityX *= 1.4;
+    this.velocityY *= 1.2;
+    this.angularVelocity = this.velocityX * 0.05;
   }
 }
 
@@ -87,8 +224,6 @@ class Head {
 
   update() {
     if (this.dragging) {
-      let dx = this.x - this.baseX;
-      let dy = this.y - this.baseY;
       this.angle = constrain(this.angle, -0.9, 0.9);
       return;
     }
@@ -96,7 +231,6 @@ class Head {
     let springX = (this.baseX - this.x) * 0.12;
     let springY = (this.baseY - this.y) * 0.12;
     let damping = 0.84;
-    
 
     this.velocityX += springX;
     this.velocityY += springY;
@@ -106,7 +240,7 @@ class Head {
     this.x += this.velocityX;
     this.y += this.velocityY;
 
-    this.angularVelocity += (this.velocityX * 0.03 - this.angle * 0.16);
+    this.angularVelocity += this.velocityX * 0.03 - this.angle * 0.16;
     this.angularVelocity *= 0.78;
     this.angle += this.angularVelocity;
 
@@ -140,8 +274,6 @@ class Head {
     ellipse(-18, -5, 10, 14);
     ellipse(18, -5, 10, 14);
 
-
-    
     pop();
   }
 
@@ -155,7 +287,9 @@ class Head {
       this.velocityX = 0;
       this.velocityY = 0;
       this.angularVelocity = 0;
+      return true;
     }
+    return false;
   }
 
   mouseDragged(mx, my) {
@@ -184,4 +318,3 @@ class Head {
     this.angularVelocity = this.velocityX * 0.05;
   }
 }
-
